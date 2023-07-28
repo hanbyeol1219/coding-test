e959e878df39b922f0a269cc94e13170e22c45ad
Lee Hanbyeol <130561433+hanbyeol1219@users.noreply.github.com>
2023-07-29 01:14:33 +0900

    [level 0] Title: 개미 군단, Time: 3.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,16 @@
+const solution = (hp) => {
+ let count1 = parseInt(hp/5);
+console.log("장군개미가 몇 마리 필요하냐면 : ", count1) 
+let count2 = hp - (count1 * 5)
+console.log("장군개미를 제외하면 남는 hp : ", count2) 
+
+let count3 = parseInt(count2/3)
+console.log("장군개미 제외 후 병정개미가 몇 마리 필요하냐면 : ", count3)
+let count4 = hp - ((count3 * 3)+(count1 *5))
+console.log("장군개미와 병정개미를 제외하면 남는 hp이자 일개미 수 : ", count4)
+
+
+
+    let answer = count1+count3+count4;
+    return answer;
+}
