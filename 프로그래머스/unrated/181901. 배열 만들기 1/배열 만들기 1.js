3c884c703ec637b0ee39dd31672369d31c4a3f5e
Lee Hanbyeol <130561433+hanbyeol1219@users.noreply.github.com>
2023-08-04 09:29:51 +0900

    [unrated] Title: 배열 만들기 1, Time: 2.63 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,7 +1,19 @@
-const solution = (n, k) => {
+/*const solution = (n, k) => {
  let answer = [];
  for(let i=k; i<=n; i+=k){
   answer.push(i)
  }
  return answer;
+}*/
+
+function solution(n, k) {
+    let answer = [];
+
+    for(let i = 1; i <= n; i++) {
+        if(i%k === 0) {
+            answer.push(i);
+        }
+    }
+
+    return answer;
 }
