02ec36687f83b99906a7a523064cd6140938db38
Lee Hanbyeol <130561433+hanbyeol1219@users.noreply.github.com>
2023-12-29 17:36:36 +0900

    [level 2] Title: 올바른 괄호, Time: 5.36 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -5,24 +5,7 @@ const solution = (s) => {
     // → ')'이면 stack에 length 체크 후 length가 0이면 return하여 넘기기
     // → length가 0이 아니면 기존에 들어있던 '('를 pop하여 '()'세트로 지우기
     // → s를 다 돌고나서 stack의 length가 0이면 bool을 true로 return
-    
-    // 1
-    let bool = false;
-    let openCnt = 0;
-    let closeCnt = 0;
-    for (let i = 0; i < s.length; i++) {
-        s[i] === '(' ? openCnt++ : closeCnt++;
-    }
-    bool = openCnt === closeCnt ? true : false;
-    console.log("'('와 ')'의 갯수가 동일한가요?", bool);
-    
-    // 2
-    if(bool) {
-        bool = s[0] !== ")" && s[s.length-1] !== "(" ? true : false
-    }
-    console.log("'('와 ')'의 갯수가 동일하고 ')'로 시작하거나 '('로 끝나지 않나요?", bool);
-    
-    if(bool) {
+        let answer = false;
         const stack = [];
         for (let i = 0; i < s.length; i++) {
             if (s[i] === '(') {
@@ -34,8 +17,8 @@ const solution = (s) => {
                 stack.pop();
             }  
         }
-        bool = stack.length === 0 ? true : false;
-    }
-    
-    return bool;
+        answer = stack.length === 0 ? true : false;
+
+
+    return answer;
 }
